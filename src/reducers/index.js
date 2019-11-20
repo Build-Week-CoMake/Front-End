893f76d52513a56497369c8770ed24e7270bce7a
Nathaniel Mosco <natemosco@gmail.com>
2019-11-20 12:01:48 -0500

    merged with William-Weng to work with updated files. no conflicts present

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,18 +2,18 @@ import axiosWithAuth from "../utils/axiosWithAuth";
 import axios from "axios";
 export const ADD_POST = "ADD";  //create a post
 export const DELETE_POST = "DELETE_POST";  //delete a post
-export const SELECT_ITEM_TO_DELETE = "SELECT_ITEM_TO_DELETE"
-export const UNSELECT_ITEM_TO_DELETE = "UNSELECT_ITEM_TO_DELETE"
-export const UPDATE_POST = "UDATE_POST";  //update a post
+export const DOWN_VOTE = "DOWN_VOTE"; // PUT REQUEST plus filter to reorder list of comments
+export const EDIT_THIS_ISSUE = "EDIT_THIS_ISSUE";
+export const GET_BY_LOCATION = "GET_BY_LOCATION";
 export const INIT_HOME = "INIT_HOME";  // get all posts for home page dashboard
 export const INIT_PROFILE = "INIT_PROFILE";  // get all posts for home page dashboard  ie: upvoted posts and created posts
-export const UP_VOTE = "UP_VOTE"; // PUT REQUEST plus filter to reorder list of comments
-export const DOWN_VOTE = "DOWN_VOTE"; // PUT REQUEST plus filter to reorder list of comments
 export const LOGOUT = "LOGOUT"
-export const TOGGLE_FORM = "TOGGLE_FORM";
-export const EDIT_THIS_ISSUE = "EDIT_THIS_ISSUE";
 export const RESET_ISSUE_TO_EDIT = "RESET_ISSUE_TO_EDIT"
-export const GET_BY_LOCATION = "GET_BY_LOCATION";
+export const SELECT_ITEM_TO_DELETE = "SELECT_ITEM_TO_DELETE"
+export const TOGGLE_FORM = "TOGGLE_FORM";
+export const UNSELECT_ITEM_TO_DELETE = "UNSELECT_ITEM_TO_DELETE"
+export const UP_VOTE = "UP_VOTE"; // PUT REQUEST plus filter to reorder list of comments
+export const UPDATE_POST = "UDATE_POST";  //update a post
 
 
 const initialState = {
@@ -72,45 +72,12 @@ const initialState = {
 };
 const appReducer = (state, action) => {
     switch (action.type) {
-        case SELECT_ITEM_TO_DELETE:
-            return {
-                ...state,
-                deleteQueue: action.payload
-            }
-        case UNSELECT_ITEM_TO_DELETE:
-            console.log("unselect item to delete is firing")
-            return {
-
-                ...state,
-                deleteQueue: {}
-            }
-        case EDIT_THIS_ISSUE:
-            return {
-                ...state,
-                issueToEdit: action.payload
-            }
-        case RESET_ISSUE_TO_EDIT:
-            console.log("reset issue to edit is firing")
-            return {
-                ...state,
-                issueToEdit: {}
-            }
-        case TOGGLE_FORM:
-            return {
-                ...state,
-                showForm: !state.showForm
-            }
-        case GET_BY_LOCATION:
-            return {
-                ...state,
-                issues: action.payload
-            }
         case ADD_POST:
             return {
                 ...state,
                 issues: action.payload,
                 showForm: false
-            }
+            };
         case DELETE_POST:
             axiosWithAuth()
                 .delete(`/issues/${action.payload}`)
@@ -125,20 +92,22 @@ const appReducer = (state, action) => {
                     console.log(err, "error from DELETE_POST function");
                 });
             break;
-        case UPDATE_POST:
-            axiosWithAuth()
-                .put(`/issues/${action.payload.id}`, action.payload)
-                .then(res => {
-                    console.log(res, "response from UPDATE_POST function");
-                    return {
-                        ...state,
-                        issues: res.data
-                    }
-                })
-                .catch(err => {
-                    console.log(err, "error from UPDATE_POST function");
-                });
-            break;
+        case DOWN_VOTE:
+            return {
+                ...state,
+                issues: action.payload,
+                profile: action.payload
+            }
+        case EDIT_THIS_ISSUE:
+            return {
+                ...state,
+                issueToEdit: action.payload
+            }
+        case GET_BY_LOCATION:
+            return {
+                ...state,
+                issues: action.payload
+            };
         case INIT_HOME:
             return {
                 ...state,
@@ -159,27 +128,55 @@ const appReducer = (state, action) => {
                     console.log(err, "error from ADD_POST function");
                 });
             break;
+        case LOGOUT:
+            localStorage.clear("token");
+            action.payload.history.push("/login");
+            break;
+        case RESET_ISSUE_TO_EDIT:
+            console.log("reset issue to edit is firing")
+            return {
+                ...state,
+                issueToEdit: {}
+            };
+        case SELECT_ITEM_TO_DELETE:
+            return {
+                ...state,
+                deleteQueue: action.payload
+            };
+        case TOGGLE_FORM:
+            return {
+                ...state,
+                showForm: !state.showForm
+            };
+        case UNSELECT_ITEM_TO_DELETE:
+            console.log("unselect item to delete is firing")
+            return {
+
+                ...state,
+                deleteQueue: {}
+            };
         case UP_VOTE:
             return {
                 ...state,
                 issues: action.payload,
                 profile: action.payload
-            }
-
-        case DOWN_VOTE:
-            return {
-                ...state,
-                issues: action.payload,
-                profile: action.payload
-            }
-
-
-        case LOGOUT:
-            localStorage.clear("token");
-            action.payload.history.push("/login");
+            };
+        case UPDATE_POST:
+            axiosWithAuth()
+                .put(`/issues/${action.payload.id}`, action.payload)
+                .then(res => {
+                    console.log(res, "response from UPDATE_POST function");
+                    return {
+                        ...state,
+                        issues: res.data
+                    }
+                })
+                .catch(err => {
+                    console.log(err, "error from UPDATE_POST function");
+                });
             break;
         default:
-            return state
-    }
-}
+            return state;
+    };
+};
 export { appReducer, initialState }
